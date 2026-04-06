483725c7f215283cdf3027e0dcfc3b4ceb5752aa
Sa1kxd <samildelacruz1@gmail.com>
2026-04-06 17:58:45 -0400

    Contexto creado

---
+++ CalculadoraDosificacionHormigon/Data/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CalculadoraDosificacionHormigon.Modelo;
+namespace CalculadoraDosificacionHormigon.Data;
+
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+    public DbSet<CalculoHormigon> CalculosHormigon { get; set; }
+}
