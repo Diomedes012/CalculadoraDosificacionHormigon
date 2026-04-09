6d9ee1f60bb7e749cdfa8258912bba932da721b3
Sa1kxd <samildelacruz1@gmail.com>
2026-04-08 20:51:46 -0400

    Modelo necesario

---
+++ CalculadoraDosificacionHormigon/Modelo/CalculoHormigon.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculadoraDosificacionHormigon.Modelo;
+
+public class CalculoHormigon
+{
+    [Key]
+    public int CalculoId { get; set; }
+    public DateTime FechaCalculo { get; set; } = DateTime.Now;
+    public double Cemento { get; set; }
+    public double Agua { get; set; }
+    public double Arena { get; set; }
+    public double Grava { get; set; }
+    public double VolumenTotal { get; set; }
+    public double RelacionAguaCemento { get; set; }
+    public double ResistenciaEstimada { get; set; }
+    public bool Eliminado { get; set; } = false;
+
+}
